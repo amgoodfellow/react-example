0e84258f7d569a1cf8c7d899581ca93d35124e93
Aaron Goodfellow <amgoodfellow@oakland.edu>
2019-11-14 17:10:37 -0500

    Updates root README and App.js #1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-// We import React and React.Component from the react package
+// We import React and React.Component to use JSX and React's lifecycle methods
+// Note: React.Component is used for /class-based/ React Components
+//       If we were using React Hooks, we wouldn't need to import Component
 import React, { Component } from 'react';
 // All this other nice stuff comes from material-ui
-// They're components with built-in functionality and styles
+// They're components with built-in functionality and style
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -14,37 +16,38 @@ class App extends Component {
   // Every Class has this thing called 'state' - it's what makes React so amazing.
   // Basically, React keeps track of whenever a state variable changes, and when it does
   // it re-renders anything necessary to show that change on the page. The alternative
-  // would be keeping track yourself
+  // would be keeping track of everything that /could/ change all by yourself
   state = { open: false, name: '' }
 
   // This is the same as a private method in Java. You might see some people using a syntax like:
-  //  function handleClose(){...}
+  // function handleClose(){...}
   // This is a bit of personal preference, but typically fat-arrow functions are nicer to work
-  // with. This weird arrow function (shown below) binds the method //to this class, which is 
+  // with. This weird arrow function (shown below) binds the method to this class, which is
   // what most Java people are used to happening automatically.
+  // Inside the method, we need to call this.setState() to change any variable we previously
+  // declared in state up above. this.setState is given to us because we extend Component
   hanldeClick = () => {
-    // To change state, you have to call the setState method
-    // It accepts an object as a parameter
     this.setState({ open: !this.state.open })
   }
 
-  // This method is called when the text field is changed. It passes an `event` which we can get the
-  // target value of
+  // This method takes an event as a paramenter. In this case, the event is going to be when
+  // our 'name' TextField changes. From the event, we can get the target value, which is what's
+  // actually inside of the text field due to that event
   handleChange = event => {
     this.setState({ name: event.target.value })
   }
 
 
-  // Because this class extends React.Component, it inherits several lifecylce methods.
+  // Because this class extends React.Component, it inherits several lifecycle methods.
   // One that shows up in basically every class is `render` - this is where one typically
   // puts all of the JSX you want to see on the page
   render() {
     return (
       // There has to be one parent container - you can't render two top-level components
-      // In this case that countainer is a div
+      // In this case that countainer is a div. You could also use a `Fragment` or just `<></>`
       <div>
         <Button color="primary" onClick={this.hanldeClick}>
-          Open stuff
+          Open
         </Button>
         <TextField
           id="name"
--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-//Every file needs to import react so we have access to all its classes/lifecycle methods
+// Every file needs to import react so we have access to all its classes/lifecycle methods
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// These imports are from Material-UI a component and style library we use in all of our
+// react projects. We can create a default theme on a per-project basis with MUI's function
+// createMuiTheme
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 
-//This needs to be called in order for the project to insert itself into the HTML
-//`document.getElementById` is vanilla JavaScript. In your HTML, you have to have some
-//element with the id matching the one in here. React will render itself in that element
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// To keep the look of all of our frontends consistent, we define the same palette for each
+// project. That way the color scheme is always consistent
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  },
+  palette: {
+    primary: {
+      light: '#b89f74',
+      main: '#877148',
+      dark: '#58461f',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#56a2ea',
+      main: '#0074b7',
+      dark: '#004987',
+      contrastText: '#fff'
+    }
+  }
+})
+
+// To allow for easy copy/pasting, root element id's correspond to project name
+const project_name = 'react-example'
+
+
+// This needs to be called in order for the project to insert itself into our HTML page.
+// `document.getElementById` is vanilla JavaScript. In your HTML, you'll have to have some
+// element with the id matching the one in here. React will render itself inside that element.
+// When you run an `npm start`, the project renders itself in the file found in
+// `../public/index.html`
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById(project_name));
